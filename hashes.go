ba3601ef899dbe59aa290c9e41c0e6ede9c04432
Victor Deryagin <vderyagin@gmail.com>
2013-06-06 15:39:26 +0300

    work for single file only

--- hashes.go
+++ hashes.go
@@ -22,44 +22,40 @@ type h struct {
 }
 
 func main() {
-	files := os.Args[1:]
+	fileName := os.Args[1]
 
-	for _, file := range files {
-		f, err := os.Open(file)
+	f, err := os.Open(fileName)
 
-		if err != nil {
-			log.Fatalf("could not open file %s\n", file)
-		}
+	if err != nil {
+		log.Fatalf("could not open file %s\n", fileName)
+	}
 
-		hashes := []h{
-			h{name: "md5", hash: md5.New()},
-			h{name: "sha1", hash: sha1.New()},
-			h{name: "sha256", hash: sha256.New()},
-			h{name: "sha512", hash: sha512.New()},
-			h{name: "adler32", hash: adler32.New()},
-			h{name: "crc32 (IEEE)", hash: crc32.New(crc32.MakeTable(crc32.IEEE))},
-			h{name: "crc32 (Castagnoli)", hash: crc32.New(crc32.MakeTable(crc32.Castagnoli))},
-			h{name: "crc32 (Koopman)", hash: crc32.New(crc32.MakeTable(crc32.Koopman))},
-			h{name: "crc64 (ISO)", hash: crc64.New(crc64.MakeTable(crc64.ISO))},
-			h{name: "crc64 (ECMA)", hash: crc64.New(crc64.MakeTable(crc64.ECMA))},
-			h{name: "fnv32-1", hash: fnv.New32()},
-			h{name: "fnv32-1a", hash: fnv.New32a()},
-			h{name: "fnv64-1", hash: fnv.New64()},
-			h{name: "fnv64-1a", hash: fnv.New64a()},
-		}
+	hashes := []h{
+		h{name: "md5", hash: md5.New()},
+		h{name: "sha1", hash: sha1.New()},
+		h{name: "sha256", hash: sha256.New()},
+		h{name: "sha512", hash: sha512.New()},
+		h{name: "adler32", hash: adler32.New()},
+		h{name: "crc32 (IEEE)", hash: crc32.New(crc32.MakeTable(crc32.IEEE))},
+		h{name: "crc32 (Castagnoli)", hash: crc32.New(crc32.MakeTable(crc32.Castagnoli))},
+		h{name: "crc32 (Koopman)", hash: crc32.New(crc32.MakeTable(crc32.Koopman))},
+		h{name: "crc64 (ISO)", hash: crc64.New(crc64.MakeTable(crc64.ISO))},
+		h{name: "crc64 (ECMA)", hash: crc64.New(crc64.MakeTable(crc64.ECMA))},
+		h{name: "fnv32-1", hash: fnv.New32()},
+		h{name: "fnv32-1a", hash: fnv.New32a()},
+		h{name: "fnv64-1", hash: fnv.New64()},
+		h{name: "fnv64-1a", hash: fnv.New64a()},
+	}
 
-		writers := make([]io.Writer, len(hashes))
+	writers := make([]io.Writer, len(hashes))
 
-		for idx := range hashes {
-			writers[idx] = hashes[idx].hash
-		}
+	for idx := range hashes {
+		writers[idx] = hashes[idx].hash
+	}
 
-		io.Copy(io.MultiWriter(writers...), f)
+	io.Copy(io.MultiWriter(writers...), f)
 
-		fmt.Printf("%s:\n", file)
-
-		for _, hsh := range hashes {
-			fmt.Printf("%s: %x\n", hsh.name, hsh.hash.Sum(nil))
-		}
+	for _, hsh := range hashes {
+		fmt.Printf("%s: %x\n", hsh.name, hsh.hash.Sum(nil))
 	}
 }
