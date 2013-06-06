41d6e11ac7beaf74533b74144ff57356ffbc2fe7
Victor Deryagin <vderyagin@gmail.com>
2013-06-06 15:28:39 +0300

    refactored, added more kinds of crc32

--- hashes.go
+++ hashes.go
@@ -16,20 +16,10 @@ import (
 	"os"
 )
 
-var format = `md5:          %x
-sha1:         %x
-sha256:       %x
-sha512:       %x
-adler32:      %x
-crc32 (IEEE): %x
-crc64 (ECMA): %x
-crc64 (ISO):  %x
-fnv32:        %x
-fnv32a:       %x
-fnv64:        %x
-fnv64a:       %x
-
-`
+type h struct {
+	name string
+	hash hash.Hash
+}
 
 func main() {
 	files := os.Args[1:]
@@ -41,30 +31,35 @@ func main() {
 			log.Fatalf("could not open file %s\n", file)
 		}
 
-		hashes := []io.Writer{
-			md5.New(),
-			sha1.New(),
-			sha256.New(),
-			sha512.New(),
-			adler32.New(),
-			crc32.NewIEEE(),
-			crc64.New(crc64.MakeTable(crc64.ISO)),
-			crc64.New(crc64.MakeTable(crc64.ECMA)),
-			fnv.New32(),
-			fnv.New32a(),
-			fnv.New64(),
-			fnv.New64a(),
+		hashes := []h{
+			h{name: "md5", hash: md5.New()},
+			h{name: "sha1", hash: sha1.New()},
+			h{name: "sha256", hash: sha256.New()},
+			h{name: "sha512", hash: sha512.New()},
+			h{name: "adler32", hash: adler32.New()},
+			h{name: "crc32 (IEEE)", hash: crc32.New(crc32.MakeTable(crc32.IEEE))},
+			h{name: "crc32 (Castagnoli)", hash: crc32.New(crc32.MakeTable(crc32.Castagnoli))},
+			h{name: "crc32 (Koopman)", hash: crc32.New(crc32.MakeTable(crc32.Koopman))},
+			h{name: "crc64 (ISO)", hash: crc64.New(crc64.MakeTable(crc64.ISO))},
+			h{name: "crc64 (ECMA)", hash: crc64.New(crc64.MakeTable(crc64.ECMA))},
+			h{name: "fnv32-1", hash: fnv.New32()},
+			h{name: "fnv32-1a", hash: fnv.New32a()},
+			h{name: "fnv64-1", hash: fnv.New64()},
+			h{name: "fnv64-1a", hash: fnv.New64a()},
 		}
 
-		io.Copy(io.MultiWriter(hashes...), f)
+		writers := make([]io.Writer, len(hashes))
 
-		var sums = make([]interface{}, len(hashes))
-
-		for idx, hsh := range hashes {
-			sums[idx] = hsh.(hash.Hash).Sum(nil)
+		for idx := range hashes {
+			writers[idx] = hashes[idx].hash
 		}
+
+		io.Copy(io.MultiWriter(writers...), f)
 
 		fmt.Printf("%s:\n", file)
-		fmt.Printf(format, sums...)
+
+		for _, hsh := range hashes {
+			fmt.Printf("%s: %x\n", hsh.name, hsh.hash.Sum(nil))
+		}
 	}
 }
